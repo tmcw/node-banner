77faa1ee61b159c9a7cc87d951d8188d5cb53758
Tom MacWright <macwright@gmail.com>
2011-07-08 10:24:09 -0400

    Git describe instead of parsing git log

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ creates headers automatically. Use it in a makefile, like
 
     dist_setup:
         mkdir dist
-        $(BANNER) package.json dist/header.js
+        $(BANNER) package.json > dist/header.js
 
 And it'll be easier to keep track of versions and commits in
 minified files. It tracks both `package.json` versions and git commits,
--- bin/banner.js
+++ bin/banner.js
@@ -5,22 +5,19 @@ var fs = require('fs');
     spawn = require('child_process').spawn,
     argv = process.argv.slice(2, process.argv.length);
 
-if (argv.length<2) return sys.error('usage:\n\tbanner package.json header.js');
+if (argv.length !== 1) return sys.error('usage:\n\tbanner package.json > header.js');
 
 var package = JSON.parse(fs.readFileSync(argv[0], 'utf-8')),
-    git = spawn('git', ['--git-dir', path.join(process.cwd(), '.git'), 'show']);
+    git = spawn('git', ['describe']),
     version = package.version,
     name = package.name,
     width = name.length + version.length,
     commit = '';
 
-
 git.stdout.on('data', function(d) {
-    commit = ' - ' + d.toString().split(/\s/)[1].substr(0,10);
+    commit = ' - ' + d.toString().trim();
 });
 
 git.on('exit', function() {
-    fs.writeFileSync(argv[1],
-        '/* ' + name + ' - ' + version + commit + ' */',
-        'utf-8');
+    console.log('/* ' + name + ' - ' + version + commit + ' */\n\n');
 });
